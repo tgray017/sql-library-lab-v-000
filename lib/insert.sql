18186bb6ae0e487681f2f8bcfd1c924427f96d1f
tgray017 <tgray017@gmail.com>
2019-01-24 13:14:41 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES 
+  ("A Song of Ice and Fire", 1, 1),
+  ("Harry Potter", 2, 1)
+;
+
+
+INSERT INTO books (title, year, series_id) VALUES 
+  ("A Song of Ice and Fire", 1, 1),
+;
